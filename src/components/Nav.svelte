ddbcb593c4aeb4a5c77c61cb5ee6dcdad1d28b57
KAEL <vincent.achy@gmail.com>
2024-01-24 14:57:05 +0100

    Initial commit

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,18 +1,25 @@
 <!-- nav.svelte -->
 
-<div class="nav-container">
+<nav class="navbar">
+	<input id="toggle" type="checkbox" />
+	<label for="toggle" class="hamburger">
+		<div class="top-bun"></div>
+		<div class="meat"></div>
+		<div class="bottom-bun"></div>
+	</label>
+
 	<div class="nav-links">
-		<a href="/">FR</a>
+		<a href="#home">Home</a>
+		<a href="#about">About</a>
+		<a href="#projects">Projects</a>
+		<a href="#contact">Contact</a>
 	</div>
-	<div class="logo">
-		<p>AMA</p>
+	<div class="social-links">
+		<a href="#instagram">Instagram</a>
+		<a href="#linkedin">LinkedIn</a>
 	</div>
-	<div class="menu-icon">
-		<div class="bar"></div>
-		<div class="bar"></div>
-		<div class="bar"></div>
-	</div>
-</div>
+	<p class="copy-right">FSA © All Rights Reserved. 2023</p>
+</nav>
 
 <style>
 	@import '../styles/nav.scss';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <!-- HOME -->
-
 <div class="topHome">
 	<div class="title">
 		<h1>Matthieu Achy</h1>
 		<h2>Architecture</h2>
 	</div>
-
 	<a href="#section-below" class="arrow-down">
 		<i class="fa-solid fa-arrow-down"></i>
 	</a>
