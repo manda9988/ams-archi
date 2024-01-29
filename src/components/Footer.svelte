cc119f294818bd9d205a74191472e5db680f7dee
KAEL <vincent.achy@gmail.com>
2024-01-29 19:35:26 +0100

    Initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,32 @@
+<footer>
+	<div class="footer-container">
+		<div class="address-section">
+			<h2>ADDRESS</h2>
+			<p>ESPAI ALFARO</p>
+			<p>AV. ESCULTOR ANDREU ALFARO 13</p>
+			<p>46110 GODELLA, VALENCIA</p>
+			<p>SPAIN</p>
+		</div>
+		<div class="contact-section">
+			<h2>CONTACT</h2>
+			<p>FRAN SILVESTRE ARQUITECTOS</p>
+			<p>
+				<a href="mailto:INFO@FRANSILVESTREARQUITECTOS.COM">INFO@FRANSILVESTREARQUITECTOS.COM</a>
+			</p>
+			<p><a href="tel:+34963816561">+34 963 81 65 61</a></p>
+		</div>
+		<div class="social-section">
+			<h2>FOLLOW US</h2>
+			<ul>
+				<li><a href="https://instagram.com">INSTAGRAM</a></li>
+				<li><a href="https://twitter.com">TWITTER</a></li>
+				<li><a href="https://linkedin.com">LINKEDIN</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="copy-right">©AV.Prod. All Rights Reserved. 2024</div>
+</footer>
+
+<style>
+	@import '../styles/footer.scss';
+</style>
