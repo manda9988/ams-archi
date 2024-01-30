597b0d989bc54890b54412d00c2e42c447132184
KAEL <vincent.achy@gmail.com>
2024-01-30 17:34:53 +0100

    Initial commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,19 +1,17 @@
+<!-- footer.svelte -->
 <footer>
 	<div class="footer-container">
 		<div class="address-section">
 			<h2>ADDRESS</h2>
-			<p>ESPAI ALFARO</p>
-			<p>AV. ESCULTOR ANDREU ALFARO 13</p>
-			<p>46110 GODELLA, VALENCIA</p>
-			<p>SPAIN</p>
+			<p>12, AVENUE ANDREU ALFARO</p>
+			<p>46110 VALENCIA</p>
+			<p>GUADELOUPE</p>
 		</div>
 		<div class="contact-section">
 			<h2>CONTACT</h2>
-			<p>FRAN SILVESTRE ARQUITECTOS</p>
-			<p>
-				<a href="mailto:INFO@FRANSILVESTREARQUITECTOS.COM">INFO@FRANSILVESTREARQUITECTOS.COM</a>
-			</p>
-			<p><a href="tel:+34963816561">+34 963 81 65 61</a></p>
+			<p>MATTHIEU ACHY ARCHITECTURE</p>
+			<p>ACHYSAMMATTHIEU@GMAIL.COM</p>
+			<p>+590 690 30 81 20</p>
 		</div>
 		<div class="social-section">
 			<h2>FOLLOW US</h2>
