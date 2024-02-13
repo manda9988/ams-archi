661f316017ef6583c9dd177da82cff6a765c3495
KAEL <vincent.achy@gmail.com>
2024-02-13 16:45:29 +0100

    commit

--- src/lib/projectsData.js
+++ src/lib/projectsData.js
@@ -11,22 +11,14 @@ export const projects = [
 			'DescriptionBis Project 1, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis.',
 		descriptionTer:
 			'DescriptionTer Project 1, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis. Saepe mollitia beatae, earum sit consequatur ut magni tempora suscipit.',
-		imageUrl: '/images/img1.jpeg',
+		imageUrl: '/images/imgPro1.jpeg',
 		images: [
-			'/images/imgProj/pro1-Img1.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img3.png'
-			// '/images/imgProj/pro1-Img4.png'
+			'/images/img1.jpeg',
+			'/images/img2.jpeg',
+			'/images/img3.jpeg',
+			'/images/img12.png',
+			'/images/img11.png',
+			'/images/img10.png'
 		]
 	},
 	{
@@ -39,49 +31,14 @@ export const projects = [
 			'DescriptionBis Project 2, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis.',
 		descriptionTer:
 			'DescriptionTer Project 2, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis. Saepe mollitia beatae, earum sit consequatur ut magni tempora suscipit.',
-		imageUrl: '/images/img2.jpeg',
+		imageUrl: '/images/imgPro2.jpeg',
 		images: [
-			'/images/imgProj/pro1-Img1.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img3.png'
-			// '/images/imgProj/pro1-Img4.png'
-		]
-	},
-	{
-		id: 'project3',
-		name: 'nameProject3',
-		nameBis: 'nameBisProject3',
-		description:
-			'Description Project 3, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis.',
-		descriptionBis:
-			'DescriptionBis Project 3, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis.',
-		descriptionTer:
-			'DescriptionTer Project 3, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis. Saepe mollitia beatae, earum sit consequatur ut magni tempora suscipit.',
-		imageUrl: '/images/img3.jpeg',
-		images: [
-			'/images/imgProj/pro1-Img1.png',
-			'/images/imgProj/pro1-Img2.png',
-			'/images/imgProj/pro1-Img3.png'
-			// '/images/imgProj/pro1-Img4.png'
-		]
-	},
-
-	{
-		id: 'project4',
-		name: 'nameProject4',
-		nameBis: 'nameBisProject4',
-		description:
-			'Description Project 4, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis.',
-		descriptionBis:
-			'DescriptionBis Project 4, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis.',
-		descriptionTer:
-			'DescriptionTer Project 4, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis. Saepe mollitia beatae, earum sit consequatur ut magni tempora suscipit.',
-		imageUrl: '/images/img4.jpeg',
-		images: [
-			'/images/imgProj/pro2-Img1.png',
-			'/images/imgProj/pro2-Img2.png',
-			'/images/imgProj/pro2-Img3.png',
-			'/images/imgProj/pro2-Img4.png'
+			'/images/img4.jpeg',
+			'/images/img5.png',
+			'/images/img6.png',
+			'/images/img7.png',
+			'/images/img8.png',
+			'/images/img9.png'
 		]
 	}
 ];
