d666bc2ed96804e771e339fcd1dbb077c147392d
KAEL <vincent.achy@gmail.com>
2024-02-13 18:31:09 +0100

    commit

--- src/lib/projectsData.js
+++ src/lib/projectsData.js
@@ -13,7 +13,7 @@ export const projects = [
 			'DescriptionTer Project 1, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis. Saepe mollitia beatae, earum sit consequatur ut magni tempora suscipit.',
 		imageUrl: '/images/imgPro1.jpeg',
 		images: [
-			'/images/img1.jpeg',
+			'/images/img1.png',
 			'/images/img2.jpeg',
 			'/images/img3.jpeg',
 			'/images/img12.png',
@@ -33,7 +33,7 @@ export const projects = [
 			'DescriptionTer Project 2, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias nulla maxime. Nulla, quaerat. Placeat totam nobis alias modi beatae eveniet neque deleniti eum asperiores dolorum consequatur maiores earum fugiat, quos ducimus aspernatur dignissimos laboriosam officiis. Saepe mollitia beatae, earum sit consequatur ut magni tempora suscipit.',
 		imageUrl: '/images/imgPro2.jpeg',
 		images: [
-			'/images/img4.jpeg',
+			'/images/img4.png',
 			'/images/img5.png',
 			'/images/img6.png',
 			'/images/img7.png',
