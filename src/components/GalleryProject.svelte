a0be0be3880c7654298962449ea792b408383692
KAEL <vincent.achy@gmail.com>
2024-02-14 15:38:57 +0100

    commit

--- src/components/GalleryProject.svelte
+++ src/components/GalleryProject.svelte
@@ -5,7 +5,7 @@
 	export let project: Project;
 </script>
 
-<section id="proProjects" class="section-projects">
+<section class="section-projectID">
 	<div class="projects-gallery">
 		{#if project && project.images}
 			{#each project.images as imageUrl}
