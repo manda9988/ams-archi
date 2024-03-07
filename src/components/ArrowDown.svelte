eda7f3f82430cd5d38304fcb8ceddeb3b939d389
KAEL <vincent.achy@gmail.com>
2024-03-07 18:10:17 +0100

    commit

--- src/components/ArrowDown.svelte
+++ src/components/ArrowDown.svelte
@@ -2,37 +2,47 @@
 	export let link = '#'; // Permet de personnaliser le lien
 </script>
 
-<a href={link} class="arrow-down">
-	<span class="fa-arrow">↓</span>
-</a>
+<a href={link} class="arrow-down"> <i class="fa-solid fa-arrow-down"></i></a>
 
 <style>
 	.arrow-down {
+		display: flex;
+		align-items: center;
+		justify-content: center;
 		cursor: pointer;
 		position: absolute;
-		bottom: 16px;
+		bottom: 20px;
 		left: 50%;
 		transform: translate(-50%);
-		color: rgb(255, 255, 255);
-		transition: all 0.6s ease-in-out;
-		font-size: 50px;
-		font-weight: 200;
+		background: rgb(237, 237, 237);
+		color: rgba(62, 62, 62, 0.744);
+		transition: all 0.4s ease-in-out;
 		user-select: none;
+		border-radius: 50%; /* Pour un cercle parfait */
+		font-size: 20px;
+		height: 30px;
+		width: 30px;
 
 		&:hover {
-			font-weight: 300;
-			/* color: rgb(255, 255, 255); */
+			color: rgb(237, 237, 237);
+			background: rgb(62, 62, 62);
 		}
 	}
 
 	@media (max-width: 768px) {
 		.arrow-down {
-			font-size: 48px;
+			bottom: 18px;
+			font-size: 18px;
+			height: 28px;
+			width: 28px;
 		}
 	}
 	@media (max-width: 550px) {
 		.arrow-down {
-			font-size: 44px;
+			bottom: 16px;
+			font-size: 16px;
+			height: 26px;
+			width: 26px;
 		}
 	}
 </style>
