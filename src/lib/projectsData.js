cec0861a0e10760eac86f68705e07a8e37add613
KAEL <vincent.achy@gmail.com>
2024-03-08 00:12:42 +0100

    commit

--- src/lib/projectsData.js
+++ src/lib/projectsData.js
@@ -5,11 +5,11 @@ export const about = {
 	name1: 'About Us', // ou tout autre valeur qui a du sens pour vous
 	name2: 'Additional Information',
 	description1:
-		"Notre agence d'architecture s'engage à créer des espaces qui allient esthétique, fonctionnalité et durabilité. Nous croyons en une architecture qui respecte l'environnement tout en répondant aux besoins spécifiques de nos clients. Notre approche est centrée sur l'écoute et la collaboration étroite avec chaque client pour transformer leur vision en réalité.",
+		"Notre agence d'architecture s'engage à créer des espaces qui allient esthétique, fonctionnalité et durabilité. Nous croyons en une architecture qui respecte l'environnement tout en répondant aux besoins spécifiques de nos clients. Notre philosophie repose sur une écoute attentive et une collaboration étroite pour concrétiser les aspirations de ceux que nous servons.",
 	descriptions: [
 		"Nous sommes convaincus que chaque projet est unique et mérite une solution sur mesure. Grâce à une combinaison d'innovation, de créativité et de rigueur technique, nous visons à concevoir des bâtiments et des espaces qui améliorent la qualité de vie des utilisateurs et s'intègrent harmonieusement à leur environnement. Notre objectif est de dépasser les attentes en livrant des projets qui se distinguent par leur qualité et leur pertinence.",
 
-		"Notre passion pour l'architecture guide notre quête d'innovation et de durabilité. Nous valorisons la collaboration étroite avec nos clients, les accompagnant de la conception à la réalisation. Ainsi, nous façonnons des espaces inspirants qui se distinguent par leur qualité et leur intégration harmonieuse dans le temps."
+		"Notre passion pour l'architecture guide notre quête d'innovation et de durabilité, ainsi, nous façonnons des espaces inspirants qui se distinguent par leur qualité et leur intégration harmonieuse dans le temps."
 	],
 	imageMain: '/images/aboutDefaultImage.png',
 	images: []
@@ -24,13 +24,11 @@ export const projects = [
 		descriptions: [
 			'De la ville au village citoyen.',
 
-			"Nous voulions apporter une touche de modernité par de nouvelles formes architecturales tout en conservant un esprit traditionnel (utilisation du matériau composite aspect bois qui rappelle les cases créoles). L'utilisation du matériau composite aspect bois sur l'ensemble de l'opération crée un lien fort entre les ilots et vise à donner à l'ensemble un esprit village.",
+			"Ce projet est l'expression d'un renouveau urbain qui tisse ensemble les fils de la modernité et de la tradition. Son développement se distingue par son approche innovante qui privilégie des matériaux naturels, rappelant l'authenticité des cases créoles, tout en introduisant des formes architecturales contemporaines. ",
 
-			"La perméabilité des ilots est le coeur du projet. Il permet via des venelles piétonnes de se rendre directement en coeur d'ilot. L'implantation du bâti et les venelles piétonnes dégagent des percées visuelles. Ces percées ventilent le coeur d'ilot. Notre démarche consiste à recréer du lien social par la rencontre. Afin de favoriser les rencontres, la majorité des ilots disposent de jardins partagés/collectifs. Nous voulions que l'habitant puisse s'approprier les espaces verts (jardins partagés). L'espace public en coeur d'ilot invite les visiteurs à échanger avec les habitants.",
+			"Centré sur la création d'espaces de vie partagés et l'encouragement des interactions sociales à travers des jardins collectifs et des venelles piétonnes, ce projet aspire à renforcer le lien communautaire et à offrir une expérience de vie enrichie au cœur d'un espace vert, urbain et dynamique. ",
 
-			"Il est conçu comme une promenade connectant les ilots 1B, 2 et 1C entre eux.Ici, l'idée de promenade est rythmée par les commerces ombragés au RDC, des activités ludiques, des parcours sportifs et un terrain de pétanque. ",
-
-			"A plus grande échelle, l'espace vert urbain et sa maison de quartier permettront de connecter les quartiers entre eux et de favoriser les échanges."
+			"Ainsi, Capou-Ferrand évolue en un écosystème dynamique, où modernité et tradition fusionnent pour réinventer la cohabitation et la convivialité au sein d'un cadre urbain revitalisé."
 		],
 		imageMain: '/images/imgMain.png',
 		images: [
