33415c74b85220031e659e7004132efe84842342
KAEL <vincent.achy@gmail.com>
2024-07-24 19:02:00 +0200

    commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,23 +2,23 @@
 <footer id="footer">
 	<div class="footer-container">
 		<div class="contact-section">
-			<p>MATTHIEU ACHY ARCHITECTURE</p>
-			<p>ACHYMATTHIEU@GMAIL.COM</p>
-			<p>+590 690 30 81 22</p>
+			<p>ams_ architects</p>
+			<p>ACMATTHIEU@GMAIL.COM</p>
+			<p>+590 690 88 77 22</p>
 		</div>
 		<div class="social-section">
 			<ul>
 				<li>
 					<a href="https://instagram.com" target="_blank" rel="noopener noreferrer">INSTAGRAM</a>
 				</li>
-				<li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer">TWITTER</a></li>
+
 				<li>
 					<a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
 				</li>
 			</ul>
 		</div>
 	</div>
-	<div class="copy-right">©MA. All Rights Reserved. 2024</div>
+	<div class="copy-right">© Vincent Achy. Tous droits réservés. 2024</div>
 </footer>
 
 <style>
