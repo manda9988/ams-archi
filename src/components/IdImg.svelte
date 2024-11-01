041e1afb149dc5aea436e3a7ad113b04e897224a
KAEL <vincent.achy@gmail.com>
2024-11-01 18:32:18 +0100

    commit

--- src/components/IdImg.svelte
+++ src/components/IdImg.svelte
@@ -3,22 +3,15 @@
 <script lang="ts">
 	import type { Project } from '../lib/types';
 	export let project: Project;
-
-	function extractImageName(imageUrl: string): string {
-		const imageName = imageUrl.split('/').pop();
-		return imageName ? imageName.replace(/%20/g, ' ') : 'Image';
-	}
 </script>
 
 <section class="idIMG">
 	<div class="projects-gallery">
-		{#if project.images && project.images.length > 0}
-			{#each project.images as imageUrl}
-				<div class="project-item">
-					<img src={imageUrl} alt={extractImageName(imageUrl)} class="project-image" />
-				</div>
-			{/each}
-		{/if}
+		{#each project.images ?? [] as imageUrl}
+			<div class="project-item">
+				<img src={imageUrl} alt="" class="project-image" />
+			</div>
+		{/each}
 	</div>
 </section>
 
