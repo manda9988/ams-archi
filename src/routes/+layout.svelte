02ec0655a8ef3bfb6604529bec7f6f994b874df9
KAEL <vincent.achy@gmail.com>
2024-11-02 17:27:46 +0100

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,8 @@
 <!-- layout.svelte -->
 
 <script>
-	// import { onMount } from 'svelte';
 	import Nav from '../components/Nav.svelte';
 	import Footer from '../components/Footer.svelte';
-	// import { setupHeightAdjustment } from '../lib/adjustHeight';
-
-	// onMount(() => {
-	// 	const cleanup = setupHeightAdjustment();
-	// 	return cleanup;
-	// });
 </script>
 
 <Nav />
